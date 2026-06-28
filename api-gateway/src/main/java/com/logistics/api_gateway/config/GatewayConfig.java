25b9f2a4a915aaf6fa989eb3f3c32c0659871bb9
mimix <amanpurohit183@gmail.com>
2026-06-28 12:55:10 +0530

    Project Concluded

---
+++ api-gateway/src/main/java/com/logistics/api_gateway/config/GatewayConfig.java
@@ -0,0 +1,31 @@
+package com.logistics.api_gateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GatewayConfig {
+
+    @Bean
+    public RouteLocator customRouteLocator (RouteLocatorBuilder builder)
+    {
+        return builder.routes()
+
+                .route("user_service", r -> r
+                        .path("/v1/api/user/**" , "/v1/api/auth/**", "/v1/api/driver/**" , "/v1/api/admin/**" , "/v1/api/internal/**")
+                        .uri("lb://USER-SERVICE"))
+
+                .route("shipment_service", r -> r
+                        .path("/v1/api/shipment/**")
+                        .uri("lb://SHIPMENT-SERVICE"))
+
+                .route("payment_service", r -> r
+                        .path("/v1/api/payment/**")
+                        .uri("lb://PAYMENT-SERVICE"))
+
+                .build();
+    }
+
+}
